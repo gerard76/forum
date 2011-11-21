713009f3b2bbc7b460bb64b5ef5fa4d2357127cd
Gerard Leijdekkers <gerard@eet.nu>
2011-11-21 08:43:15 +0100

    spec fix

--- spec/mailers/mailer_spec.rb
+++ spec/mailers/mailer_spec.rb
@@ -2,10 +2,11 @@ require "spec_helper"
 
 describe Mailer do
   describe "notification" do
-    let(:user)  { build :user                                   }
-    let(:topic) { mock(Forem::Topic, id: 1, subject: "a topic") }
-    let(:post)  { mock(Forem::Post, user: user, topic: topic)   }
-    let(:mail)  { Mailer.notification(user, post)   }
+    let(:user)  { build :user                                                 }
+    let(:forum) { mock(Forem::Forum)                                          }
+    let(:topic) { mock(Forem::Topic, id: 1, subject: "a topic", forum: forum) }
+    let(:post)  { mock(Forem::Post, user: user, topic: topic)                 }
+    let(:mail)  { Mailer.notification(user, post)                             }
     
     it "sends mail to the user's e-mail" do
       mail.to.should include(user.email)
