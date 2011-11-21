7f43f16ef6fdde161888a95365d0bde69e87527b
Gerard Leijdekkers <gerard@eet.nu>
2011-11-21 08:43:24 +0100

    If you do not deliver, it will not be send….

--- app/models/forem/post_observer.rb
+++ app/models/forem/post_observer.rb
@@ -2,7 +2,7 @@ class Forem::PostObserver < ActiveRecord::Observer
   
   def after_create(post)
     User.all.reject { |u| u.id == post.user.id }.each do |user|
-      Mailer.notification(user, post)
+      Mailer.notification(user, post).deliver
     end
   end
 end
