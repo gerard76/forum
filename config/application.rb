5be4a8b5ebc61ed4321a474e23fcb0522d48961d
Gerard Leijdekkers <gerard@eet.nu>
2013-02-01 15:23:30 +0100

    add default url option for ActionMailer

--- config/application.rb
+++ config/application.rb
@@ -53,5 +53,7 @@ module Forum
     
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.action_mailer.default_url_options = { :host => "forum.leydekkers.com" }
   end
 end
