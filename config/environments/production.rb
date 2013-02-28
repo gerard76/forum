59321fe6d4614be5c6a26ed323294b6ea5a04d01
Gerard Leijdekkers <gerard@eet.nu>
2013-02-28 15:49:19 +0100

    do not raise error on mail delivery failure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,5 +59,6 @@ Forum::Application.configure do
   config.active_support.deprecation = :notify
   
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings   = Hash[Configuration.smtp.map { |k,v| [k.to_sym, v] }]
 end
